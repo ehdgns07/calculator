aed4b2fb4437bae105b7de138899faa491f62c56
김민준 <optaricer@gmail.com>
2022-04-01 10:41:20 +0900

    #계산기/6 함수명 한글변경

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression(ex> 1 + 2): ");
+	printf("표현식을 입력해주세요.(ex> 1 + 2): ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
